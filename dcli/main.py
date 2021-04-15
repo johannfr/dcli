1aa73d132c4ffab7f99dc37595d8fbf520923654
Jóhann Friðriksson <johann.fridriksson@gmail.com>
2021-04-15 11:15:39 +0200

    🚸 Tweaks and cleanup.

--- dcli/main.py
+++ dcli/main.py
@@ -1,32 +1,29 @@
-import docker
 import click
-from subprocess import call
-from pathlib import Path
 
 
 def get_docker_images(ctx, args, incomplete):
+    import docker
+
     client = docker.from_env()
     tags = [tag for tags in [x.tags for x in client.images.list()] for tag in tags]
-    if len(incomplete) > 0:
-        return [t for t in tags if incomplete in t]
-    else:
-        return tags
+    return [t for t in tags if incomplete in t or len(incomplete) == 0]
 
 
 @click.command()
 @click.argument("imagename", type=click.STRING, autocompletion=get_docker_images)
 def main(imagename):
+    from subprocess import call
+    from pathlib import Path
+
     cwd = Path.cwd()
-    workspace = cwd
-    tree = [cwd]
-    tree.extend(cwd.parents)
-    for d in tree:
-        if all(x in [f.name for f in d.iterdir()] for x in [".git", "docker"]):
+    workspace = cwd  # Default to cwd, then see if we find a better one.
+    for d in [cwd] + [p for p in cwd.parents]:
+        if all(x in [f.name.lower() for f in d.iterdir()] for x in [".git", "docker"]):
             workspace = d
             break
-    click.secho(f"Mounting workspace: {workspace}", fg="blue")
+    click.secho(f"Mounting workspace: {workspace}", fg="yellow")
     call(
-        f"docker run --rm -it -v '{workspace}':'{workspace}' -w '{cwd}' {imagename}",
+        f"docker run --rm -it -v '{workspace}':'{workspace}' -w '{cwd}' '{imagename}'",
         shell=True,
     )
 
